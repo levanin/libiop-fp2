2359d5b0ffc5f9252d6b3d0607eda810f394988d
kc1212 <kc1212@users.noreply.github.com>
2022-12-23 11:36:55 +0100

    Add script for running experiment

---
+++ experiment.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+BIN=$1
+EXPERIMENT=$2
+PREFIX="====>"
+
+echo "proof_system,base_field,m,n,prover_time,verifier_time,proof_size"
+
+sed 1d "$EXPERIMENT" | while IFS=, read -r proof_system base_field _ m n; do
+  if [[ "$proof_system" == "limbo" ]]; then
+    continue
+  fi
+  out=$($BIN --proof_system "$proof_system" --base_field "$base_field" -m "$m" -n "$n" | grep "$PREFIX" | sed "s/$PREFIX//")
+  echo "$proof_system,$base_field,$m,$n,$out"
+done
